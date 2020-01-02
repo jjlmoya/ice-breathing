23e50a092d1a3cdaebd9f87dbbb01a72e64fee55
jjlmoya <jj.lopez@avoristravel.com>
2020-01-02 23:24:47 +0300

    Add button and routes

--- src/pages/Index.vue
+++ src/pages/Index.vue
@@ -1,6 +1,10 @@
 <template>
-  <div class="">
-    Empezar meditación
+  <div class="p-index">
+    <q-btn to="/meditation"
+          class="button"
+          color="primary"
+          label="Empezar Meditación"
+          size="lg" />
   </div>
 </template>
 
