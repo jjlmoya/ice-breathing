bc97dbb46f914d6534f82bccff66656bc4f942cc
jjlmoya <jj.lopez@avoristravel.com>
2020-01-05 19:35:35 +0300

    Add replay, helper text and end screen

--- src/components/Menu.vue
+++ src/components/Menu.vue
@@ -2,8 +2,10 @@
    <q-list>
     <q-item-label header>Menú</q-item-label>
     <Item :title="'Inicio'" :icon="'home'" :link="'/'" />
+    <!--
     <Item :title="'Configuración'" :icon="'settings'" :link="'/'" />
     <Item :title="'Soporte'" :subtitle="''" :icon="'€'" :link="'/'" />
+    -->
   </q-list>
 </template>
 
--- src/components/Progress.vue
+++ src/components/Progress.vue
@@ -1,18 +1,37 @@
 <template>
   <div class="progress">
-    <div>Ronda {{round}}</div>
-    <div class="progress__wrapper">
-    <q-circular-progress
-        reverse
-        :value="getValue()"
-        size="90px"
-        :thickness="0.2"
-        color="light-blue"
-        center-color="purple"
-        track-color="white"
-      />
+      <div class="playing" v-if="this.round <= this.maxRounds">
+      <div>Ronda {{round}} de {{maxRounds}}</div>
+      <div class="progress__wrapper">
+      <q-circular-progress
+          reverse
+          :value="getValue()"
+          size="90px"
+          :thickness="0.2"
+          color="light-blue"
+          center-color="purple"
+          track-color="white"
+        />
+        <div class="progress__content">{{value}}</div>
+      </div>
       <div class="progress__action"> {{action}} </div>
-      <div class="progress__content">{{value}}</div>
+    </div>
+    <div class="finish" v-if="!(this.round <= this.maxRounds)">
+       <div class="progress__action">
+         Meditación Acabada
+       </div>
+       <div class="progress__buttons">
+       <q-btn v-on:click="restart"
+          class="button"
+          color="primary"
+          label="Repetir"
+          size="lg" />
+       <q-btn to="/"
+          class="button"
+          color="primary"
+          label="Volver"
+          size="lg" />
+        </div>
     </div>
   </div>
 </template>
@@ -20,17 +39,18 @@
 
 const breathIn = new Audio('statics/sounds/breath-in.mp3')
 const breathOut = new Audio('statics/sounds/breath-out.mp3')
-
+const ONE_SECOND = 1000
 export default {
   name: 'Header',
   data: function () {
     return {
       state: 0,
       value: 0,
-      breathNumber: 3,
+      breathNumber: 40,
       holdTime: 120,
       action: '',
       refreshTime: 15,
+      maxRounds: 3,
       max: 0,
       round: 0
     }
@@ -50,26 +70,30 @@ export default {
       return this.value * 100 / this.getTimeByState()
     },
     holdLoop () {
-      this.action = 'Hold'
-      this.createLoop(this.holdTime, 100)
+      this.action = 'Aguanta'
+      this.createLoop(this.holdTime, ONE_SECOND)
     },
     breathLoop () {
       const breathInDuration = 2.377
-      const breathOutDuration = 2.037
-      const durationSounds = (breathInDuration + breathOutDuration) * 1000
+      const breathOutDuration = 1.337
+      const durationSounds = (breathInDuration + breathOutDuration) * ONE_SECOND
       this.round++
-      this.createLoop(this.breathNumber, durationSounds, () => {
-        breathIn.play()
-        this.action = 'In'
-        setTimeout(() => {
-          this.action = 'Out'
-          breathOut.play()
-        }, breathInDuration * 1000)
-      })
+      if (this.round <= this.maxRounds) {
+        this.createLoop(this.breathNumber, durationSounds, () => {
+          breathIn.play()
+          this.action = 'Inspira'
+          setTimeout(() => {
+            this.action = 'Expira'
+            breathOut.play()
+          }, breathInDuration * ONE_SECOND)
+        })
+      } else {
+
+      }
     },
     refreshLoop () {
-      this.action = 'In and Hold!'
-      this.createLoop(this.refreshTime, 1000)
+      this.action = '¡Inspira hondo y Aguanta!'
+      this.createLoop(this.refreshTime, ONE_SECOND)
     },
     nextLoop () {
       switch (this.state) {
@@ -88,7 +112,7 @@ export default {
       if (callback) { callback() }
       let loop = setInterval(() => {
         this.value++
-        if (this.value >= max) {
+        if (this.value > max) {
           clearInterval(loop)
           this.state = this.state !== 2 ? this.state + 1 : 0
           this.value = 0
@@ -97,6 +121,10 @@ export default {
           if (callback) callback()
         }
       }, step)
+    },
+    restart () {
+      this.round = 0
+      this.nextLoop()
     }
   }
 }
